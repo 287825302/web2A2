1080572475f416319457e6ffe956f653a77c44e3
287825302 <146327088+287825302@users.noreply.github.com>
2024-09-24 22:50:04 +0800

    Create GET method to retrieve all active fundraisers including the category from the  database.

--- concertAPI/controllerAPI/api-controller.js
+++ concertAPI/controllerAPI/api-controller.js
@@ -8,23 +8,26 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get("/", (req, res)=>{
-	connection.query("select * from FUNDRAISER", (err, records,fields)=> {
-		 if (err){
-			 console.error("Error while retrieve the data");
-		 }else{
-			 res.send(records);
-		 }
+router.get("/", (req, res) => {
+	connection.query(`SELECT f.*, c.NAME as category_name 
+    FROM FUNDRAISER f
+    JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID
+    WHERE f.active = 1;`, (err, records, fields) => {
+		if (err) {
+			console.error("Error while retrieve the data");
+		} else {
+			res.send(records);
+		}
 	})
 })
 
-router.get("/:id", (req, res)=>{
-	connection.query("select * from FUNDRAISER where FUNDRAISER_ID=" + req.params.id, (err, records,fields)=> {
-		 if (err){
-			 console.error("Error while retrieve the data");
-		 }else{
-			 res.send(records);
-		 }
+router.get("/:id", (req, res) => {
+	connection.query("select * from FUNDRAISER where FUNDRAISER_ID=" + req.params.id, (err, records, fields) => {
+		if (err) {
+			console.error("Error while retrieve the data");
+		} else {
+			res.send(records);
+		}
 	})
 })
 
